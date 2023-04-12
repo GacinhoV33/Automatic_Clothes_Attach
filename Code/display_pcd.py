6cdec2b85fab5ec4144436d5a00c4541a81609c4
fgacek@student.agh.edu.pl <fgacek@student.agh.edu.pl>
2023-04-12 20:56:52 +0200

    Delete rubbish files

--- Code/display_pcd.py
+++ Code/display_pcd.py
@@ -2,16 +2,21 @@
 # -*- coding: utf-8 -*-
 import open3d as o3d
 from Code.down_sample_pcd import down_sample_pcd
+import numpy as np
+import datetime
 
 
-def down_sample_and_display_pcd(file_path: str):
+def down_sample_and_display_pcd(file_path: str, write=False, filename="downsampled_file.pcd"):
     pcd = o3d.io.read_point_cloud(file_path)
-    downsampled_pcd = down_sample_pcd(pcd)
-    o3d.visualization.draw([downsampled_pcd], point_size=1)
+    new_pcd = down_sample_pcd(pcd)
+    o3d.visualization.draw([new_pcd], point_size=1)
+
+    if write:
+        write_pcd(new_pcd, filename)
 
 
 def write_pcd(pcd, filename, write_ascii=True):
     o3d.io.write_point_cloud("./files/pcd/downsampled/" + filename, pcd, write_ascii=write_ascii)
 
 
-down_sample_and_display_pcd('test_pcd_write.pcd')
+down_sample_and_display_pcd('./files/ply/shirt_from_fbx.ply', True)
